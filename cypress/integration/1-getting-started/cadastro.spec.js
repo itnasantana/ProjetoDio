5431c9a4ebe941ca553b8949f751819b72d09a3f
Itnã <itnasantana@gmail.com>
2022-03-04 20:44:35 -0300

    Curso da DIO

---
+++ cypress/integration/1-getting-started/cadastro.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+it('Visitando a tela de Cadastro do submarino', () => {
+
+
+cy.visit('https://cliente.submarino.com.br/minha-conta/cadastro?next=https%3A%2F%2Fwww.submarino.com.br%2F%3FWT.srch%3D1%26opn%3DGOOGLESEARCH%26epar%3Dbp_br_00_go_sch_brand_exata%26utm_medium%3Dbuscappc%26utm_source%3Dgoogle%26utm_campaign%3Dmarca%3Asuba%253bmidia%3Abuscappc%253bformato%3Abranding%253bsubformato%3A00%253bidcampanha%3Asch_brand_exata%26gclid%3DCjwKCAiAjoeRBhAJEiwAYY3nDGACj7nAgAIPDptpjUrWZbg-JODj3QPdWnd7xADggJX9i_EUnrb9OxoCzJAQAvD_BwE')
+
+
+})
